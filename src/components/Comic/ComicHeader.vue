37e559ca9337d6b55bb9120fb168992f7494a9ed
kthatoto <kthatoto@gmail.com>
2018-09-01 18:22:57 +0900

    ComicHeader

--- src/components/Comic/ComicHeader.vue
+++ src/components/Comic/ComicHeader.vue
@@ -1,9 +1,27 @@
 <template lang="pug">
 div
+  img(v-if="comic.id" :src="getThumb()")
+  h1 タイトル：{{ comic.title }}
+  h3.authors
+    span.authors__item(v-for="author in comic.authors") 著者：{{ author.name }}
+  p 説明文：{{ comic.summary }}
 </template>
 <script>
 export default {
+  props: ['comic'],
+  methods: {
+    getThumb () {
+      return `https://s3-ap-northeast-1.amazonaws.com/nagisa-intern/comic/${this.comic.id}/square_thumb.jpeg`
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
+.authors {
+  &__item {
+    &:not(:last-child):after {
+      content: ","
+    }
+  }
+}
 </style>
--- src/pages/Comic.vue
+++ src/pages/Comic.vue
@@ -1,8 +1,25 @@
 <template lang="pug">
 div
+  ComicHeader(:comic="comic")
 </template>
 <script>
+import ComicHeader from '@/components/Comic/ComicHeader'
+import api from '@/utils/Api'
 export default {
+  components: { ComicHeader },
+  data () {
+    return {
+      comic: {}
+    }
+  },
+  created () {
+    api('GET',
+      'https://api.myjson.com/bins/15kcb8',
+      {}
+    ).then(response => {
+      this.comic = response.data
+    })
+  }
 }
 </script>
 <style lang="scss" scoped>
