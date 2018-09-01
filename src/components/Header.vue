ed5d267571777a6b008d60a7814e2d1883433fb4
kthatoto <kthatoto@gmail.com>
2018-09-01 18:54:38 +0900

    Headerにhomeボタン追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
 .header
+  router-link.header__link(to="/")
+    icon.icon(name="home")
 </template>
 <script>
 export default {
@@ -7,7 +9,25 @@ export default {
 </script>
 <style lang="scss" scoped>
 .header {
-  height: 80px;
+  $height: 80px;
+  height: $height;
   background-color: #333;
+  padding: 0 50px;
+  &__link {
+    display: inline-block;
+    color: white;
+    height: 100%;
+    line-height: $height;
+    vertical-align: middle;
+    padding: 0 10px;
+    &:hover {
+      color: #bbb;
+    }
+    .icon {
+      width: 30px;
+      height: 30px;
+      vertical-align: middle;
+    }
+  }
 }
 </style>
