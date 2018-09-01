91de17107da6677b8c6d3c53309aa38c3237092a
kthatoto <kthatoto@gmail.com>
2018-09-01 16:49:09 +0900

    トップページ

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
 div
+  h2 人気のページ
+  TopPages
+  h2 コミック一覧
+  Comics
 </template>
 <script>
+import TopPages from '@/components/Top/TopPages'
+import Comics from '@/components/Top/Comics'
 export default {
+  components: { TopPages, Comics }
 }
 </script>
 <style lang="scss" scoped>
