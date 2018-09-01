1c3d5788b0c74b0463131729bb3d13a726f72562
kthatoto <kthatoto@gmail.com>
2018-09-01 20:00:55 +0900

    エピソード毎の画像表示

--- src/components/Comic/Episode.vue
+++ src/components/Comic/Episode.vue
@@ -1,9 +1,21 @@
 <template lang="pug">
 div
+  img.episode__thumb(:src="getImage()")
 </template>
 <script>
 export default {
+  props: ['episode'],
+  methods: {
+    getImage () {
+      return `https://s3-ap-northeast-1.amazonaws.com/nagisa-intern/data/${this.$route.params.comicId}/${this.episode.episode_number}/0000.jpeg`
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
+.episode {
+  &__thumb {
+    width: 200px;
+  }
+}
 </style>
